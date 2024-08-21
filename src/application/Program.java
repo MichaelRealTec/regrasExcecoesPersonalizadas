23976986544b3a3d6befbd88dac76d3bbdc75bfe
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-08-20 22:56:38 -0300

    Primeira solucao - solucao que seria a ruim - inicio

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// regrasExcecoesPersonalizadas
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		// Vamos trabalhar com data e deixar no formato - dd/MM/yyyy
+		
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		
+		
+		sc.close();
+	}
+
+}
